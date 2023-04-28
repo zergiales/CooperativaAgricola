3a6ce10709ea302fd3a9ba58e316dfdae2c9d199
sergio sanchez lopez <zergiosanchezlopez@gmail.com>
2023-04-28 18:22:56 +0200

    first commit

---
+++ Cooperativa.java
@@ -0,0 +1,33 @@
+
+/**
+ * Clase main desde la cual se gestiona la aplicación
+ * 
+ * @author Sergio 
+ * @version 1
+ */
+public class Cooperativa
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Cooperativa
+     */
+    public Cooperativa()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
