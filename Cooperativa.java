60ea0a1a84e88bb1068f2648c5d49a1f25ac52c7
sergio sanchez lopez <zergiosanchezlopez@gmail.com>
2023-04-29 19:45:13 +0200

    comentarios de nada

--- Cooperativa.java
+++ Cooperativa.java
@@ -6,11 +6,6 @@ import java.util.Scanner; //importamos la clase scanner
  * @version 1.0 
  */
 public class Cooperativa{
-
-
-
-
-
     public static void main(String [] args){
     	//objeto Productores
     	Productores p=new Productores("por defecto", 0, "12345678E", 4);
@@ -22,14 +17,27 @@ public class Cooperativa{
         String nombre = sc.nextLine();
         System.out.println("--------------------------------");
         System.out.println("Bienvenido: "+nombre);
-        System.out.println("¿Que desea hacer?");
+        System.out.println("Que desea hacer?");
         System.out.println("Pulse 1 si desea crear un productor");
         System.out.println("Pulse 2 si desea modificar un productor");
         System.out.println("Pulse 3 si desea almacenar un productor");
         System.out.print("numero a insertar:");
         int puntero = sc.nextInt(); //parametro fundamental para acceder al menu princpial
-        //un pequeño enunciado antes de cargar el switch
-        wi
+        //un pequenio enunciado antes de cargar el switch
+        
+        	if (puntero==1) {
+				System.out.println("Ha elegido crear un prodcutor");
+			}else if(puntero==2) {
+				System.out.println("Ha elegido modificar un prodcutor");
+			}else if(puntero==3) {
+				System.out.println("Ha elegido eliminar un prodcutor");
+			} 
+        	 
+        	
+        	else {
+				return 0;
+			}
+        
 
         
         //Metodo del menu
