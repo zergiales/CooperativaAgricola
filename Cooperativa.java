d2b41805b2f455c696cf398bd9d4f3dc3e6e2166
sergio sanchez lopez <zergiosanchezlopez@gmail.com>
2023-04-30 18:45:17 +0200

    menu

--- Cooperativa.java
+++ Cooperativa.java
@@ -7,12 +7,11 @@ import java.util.Scanner; //importamos la clase scanner
  */
 public class Cooperativa extends Menus{
 	
-	Cooperativa coop = new Cooperativa(); //creamos instancia y llamamos al metodo de la clase abstracta
     
 	public static void main(String [] args){
         //objetos de las clase que hereda 
     	Scanner sc= new Scanner(System.in);        
-    	
+    	Cooperativa coop = new Cooperativa(); //creamos instancia y llamamos al metodo de la clase abstracta
         System.out.print("---------Menu de cooperativa agricola--------"+
         " \n Inserte nombre de usuario:");
         String nombre = sc.nextLine();
@@ -24,35 +23,7 @@ public class Cooperativa extends Menus{
         System.out.println("3. Menu Distribuidores");
         System.out.println("4. Menu logistica");
         System.out.println("5. Menu compra");
-        System.out.print("inserte numero: ");
-        int puntero = sc.nextInt();
-        System.out.println("--------------------------------");
- 
-      //metodo para llamar a los menus
-        public void gestorMenu() {
-        	int punt= puntero;
-			switch (punt) {
-			case 1:
-				System.out.println("abriendo menu de Productores");
-				coop.menuProductores();
-		        System.out.println("--------------------------------");
-			case 2:
-				System.out.println("abriendo menu de Productos");
-				coop.menuProductos();
-		        System.out.println("--------------------------------");
-			case 3:
-				System.out.println("abriendo menu de Distribuidores");
-				coop.menuDistribuidores();
-		        System.out.println("--------------------------------");
-			case 4:
-				System.out.println("abriendo menu de Logística");
-				coop.menuLogistica();
-		        System.out.println("--------------------------------");
-			case 5:
-				System.out.println("abriendo menu de Compra");
-				coop.menuCompra();
-		        System.out.println("--------------------------------");
-        	}
+        coop.gestorMenu();
         
         }
 }
--- Menus.java
+++ Menus.java
@@ -1,4 +1,5 @@
-
+import java.util.Scanner;
+import java.util.Scanner; //importamos la clase scanner
 /**
  * Clase abstracta donde tendremos los menus que iran siendo
  * llamados desde la clase clase cooperativa.class
@@ -7,8 +8,10 @@
  * @version 1.0
  */
 public abstract class Menus{
-    
-    public void menuProductores() {
+	        
+	Scanner sc = new Scanner(System.in);
+
+	public void menuProductores() {
     	System.out.println("Pulse 1 si desea crear un productor");
         System.out.println("Pulse 2 si desea modificar un productor");
         System.out.println("Pulse 3 si desea eliminar un productor");
@@ -34,5 +37,41 @@ public abstract class Menus{
         System.out.println("Pulse 3 si desea eliminar algo de la cesta");
         System.out.println("Pulse 4 si desea pagar");
     }
+    public void gestorMenu() {
+    	System.out.print("inserte numero: ");
+    	int puntero = sc.nextInt();        
+        System.out.println("--------------------------------");
+        System.out.println("numero insertado: "+puntero);
+        System.out.println("cargando menu...");
+    	switch (puntero) {
+		case 1:
+			System.out.println("abriendo menu de Productores");
+			menuProductores();
+	        System.out.println("--------------------------------");
+	        break;
+		case 2:
+			System.out.println("abriendo menu de Productos");
+			menuProductos();
+	        System.out.println("--------------------------------");
+	        break;
+		case 3:
+			System.out.println("abriendo menu de Distribuidores");
+			menuDistribuidores();
+	        System.out.println("--------------------------------");
+	        break;
+		case 4:
+			System.out.println("abriendo menu de Logística");
+			menuLogistica();
+	        System.out.println("--------------------------------");
+	        break;
+		case 5:
+			System.out.println("abriendo menu de Compra");
+			menuCompra();
+	        System.out.println("--------------------------------");
+	        break;
+		default:
+			break;
+		}
     
+}
 }
